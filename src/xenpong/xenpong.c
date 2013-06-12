8a8a286c6b36bc0ff000d707086fd62d7838de11
Matthew Portas <matthew.portas@citrix.com>
2013-06-12 16:04:31 +0100

    A a timer to the Store polling

--- src/xenpong/xenpong.c
+++ src/xenpong/xenpong.c
@@ -409,16 +409,28 @@ ReadChnFromStore(
     __in PDEVICE_OBJECT DeviceObject
     )
 {
+    KTIMER Timer;
+    LARGE_INTEGER timeout = {0};
     PDEVICE_EXTENSION pdx;
     PCHAR Buffer;
     ULONG DomId;
     ULONG Port;
     NTSTATUS status;
 
+    ASSERT(KeGetCurrentIrql() <= DISPATCH_LEVEL);
+    KeInitializeTimerEx(&Timer, SynchronizationTimer);
+    KeSetTimerEx(&Timer, timeout, 500, NULL);
+
     pdx = (PDEVICE_EXTENSION) DeviceObject->DeviceExtension;
 
     Warning("Reading from Store.\n");
     for (;;) {
+
+        (VOID) KeWaitForSingleObject(&Timer,
+                                     Executive,
+                                     KernelMode,
+                                     FALSE,
+                                     NULL);
 
         STORE(Acquire, pdx->StoreInterface);
 
@@ -452,6 +464,8 @@ ReadChnFromStore(
         Warning("Read vmping/port.\n");
         Port = strtoul(Buffer, NULL, 10);
         STORE(Free, pdx->StoreInterface, Buffer);
+
+        KeCancelTimer(&Timer);
 
         STORE(Release, pdx->StoreInterface);
         break;
